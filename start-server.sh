e2044c03eae87130606c9aef233b1a9149bd551c
bsski <50927653+BSski@users.noreply.github.com>
2022-05-10 03:38:38 +0200

    start-server.sh fix

--- start-server.sh
+++ start-server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-(cd booksapi; python manage.py makemigrations; python manage.py migrate)
+(cd mysite; python manage.py makemigrations; python manage.py migrate)
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
-    (cd booksapi; python manage.py createsuperuser --no-input)
+    (cd mysite; python manage.py createsuperuser --no-input)
 fi
-(cd booksapi; gunicorn movies.wsgi:application --bind 0.0.0.0:$PORT --workers 3)
+(cd mysite; gunicorn movies.wsgi:application --bind 0.0.0.0:$PORT --workers 3)

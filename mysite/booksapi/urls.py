a8b528baf2b47bdc9d53b33a92a4324d9c7b16d4
bsski <50927653+BSski@users.noreply.github.com>
2022-05-11 02:44:29 +0200

    style fix

--- mysite/booksapi/urls.py
+++ mysite/booksapi/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 urlpatterns = [
     path("books/", views.view_books_list, name="get_books"),
-    path("books/<str:id>", views.view_book),
+    path("books/<str:book_id>", views.view_book),
     path("db/", views.post_books_to_database, name="post_books"),
     path("", RedirectView.as_view(pattern_name="get_books")),
 ]

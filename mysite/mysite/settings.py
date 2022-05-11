43764f4b31ec5564af0dae5b0b3ba4f9499bb32e
bsski <50927653+BSski@users.noreply.github.com>
2022-05-11 03:29:20 +0200

    deploy check fixes

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -143,11 +143,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CSRF_TRUSTED_ORIGINS = ["https://books-api-bsski.herokuapp.com"]
 
-#CSRF_COOKIE_SECURE = True
-
-#SESSION_COOKIE_SECURE = True
-
-#SECURE_SSL_REDIRECT = False
+CSRF_COOKIE_SECURE = os.environ["CSRF_COOKIE_SECURE"]
+SESSION_COOKIE_SECURE = os.environ["SESSION_COOKIE_SECURE"]
+SECURE_SSL_REDIRECT = os.environ["SECURE_SSL_REDIRECT"]
 
 
 django_on_heroku.settings(locals(), test_runner=False)

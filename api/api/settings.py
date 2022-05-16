38a6b311b86adf8cfa42474d0f616466304bd159
bsski <50927653+BSski@users.noreply.github.com>
2022-05-16 23:00:45 +0200

    static dir refactor

--- api/api/settings.py
+++ api/api/settings.py
@@ -168,7 +168,7 @@ STATIC_URL = "/static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "../static"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 

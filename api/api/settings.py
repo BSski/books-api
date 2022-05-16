a88193a4dde89399015b1c0192ee72122c92d6b3
bsski <50927653+BSski@users.noreply.github.com>
2022-05-16 13:31:06 +0200

    static files fix

--- api/api/settings.py
+++ api/api/settings.py
@@ -20,7 +20,7 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-dotenv_file = os.path.join(BASE_DIR, "..\.env")
+dotenv_file = os.path.join(BASE_DIR, "../.env")
 if os.path.isfile(dotenv_file):
     load_dotenv(dotenv_file)
 
@@ -138,9 +138,7 @@ STATIC_URL = "static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "../../static"),)
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "../static"),)
 
 
 # Default primary key field type

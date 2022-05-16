f4b133136d0bc2b4d874940f9608863494149d07
bsski <50927653+BSski@users.noreply.github.com>
2022-05-17 00:12:39 +0200

    style tweaks

--- start-server.sh
+++ start-server.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-(cd api; python manage.py makemigrations; python manage.py migrate; python manage.py collectstatic --no-input)
+(cd api; python manage.py makemigrations; python manage.py migrate;
+  python manage.py collectstatic --no-input)
 
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
-    (cd api; python manage.py createsuperuser --no-input)
+  (cd api; python manage.py createsuperuser --no-input)
 fi
 (cd api; gunicorn api.wsgi:application --bind 0.0.0.0:$PORT --workers 3)
